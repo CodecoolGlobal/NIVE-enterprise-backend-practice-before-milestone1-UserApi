80f9fefc594cad8fe4ceeb6bf0895ac9a90fd8e3
Adam Szentirmai-Schon <adam.szentirmai-schon@codecool.com>
2022-05-10 09:21:01 +0200

    add User model

---
+++ src/main/java/com/codecool/model/User.java
@@ -0,0 +1,42 @@
+package com.codecool.model;
+
+import java.util.List;
+
+public class User {
+    private Long id;
+    private String name;
+    private boolean enabled;
+    private List<String> roles;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public List<String> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<String> roles) {
+        this.roles = roles;
+    }
+}
