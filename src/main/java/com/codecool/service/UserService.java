7a914f9af8b1e99c484bcd60fd7826731cbc06a6
Adam Szentirmai-Schon <adam.szentirmai-schon@codecool.com>
2022-05-10 09:33:37 +0200

    implement UserService

--- src/main/java/com/codecool/service/UserService.java
+++ src/main/java/com/codecool/service/UserService.java
@@ -1,4 +1,31 @@
 package com.codecool.service;
 
+import com.codecool.model.User;
+import com.codecool.repository.InMemoryUserDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class UserService {
+
+    @Autowired
+    InMemoryUserDao userDao;
+
+    public List<User> findAll() {
+        return userDao.findAll();
+    }
+
+    public Long save(User user) {
+        return userDao.save(user);
+    }
+
+    public User findById(Long id) {
+        return userDao.findById(id);
+    }
+
+    public void deleteById(Long id) {
+        userDao.deleteById(id);
+    }
 }
