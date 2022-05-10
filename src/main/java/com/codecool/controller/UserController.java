a73199d6fe31a87447d3e01458a2e7aa718f6ecf
Adam Szentirmai-Schon <adam.szentirmai-schon@codecool.com>
2022-05-10 10:49:26 +0200

    autowire through constructor injection

--- src/main/java/com/codecool/controller/UserController.java
+++ src/main/java/com/codecool/controller/UserController.java
@@ -4,7 +4,6 @@ import com.codecool.model.User;
 import com.codecool.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
@@ -15,10 +14,13 @@ import java.util.List;
 @RestController
 public class UserController {
 
-    @Autowired
-    private UserService userService;
+    private final UserService userService;
 
     Logger logger = LoggerFactory.getLogger(UserController.class);
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping("/users")
     public List<User> findAll() {
--- src/main/java/com/codecool/service/UserService.java
+++ src/main/java/com/codecool/service/UserService.java
@@ -2,7 +2,6 @@ package com.codecool.service;
 
 import com.codecool.model.User;
 import com.codecool.repository.Dao;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,8 +9,11 @@ import java.util.List;
 @Service
 public class UserService {
 
-    @Autowired
-    Dao<User> userDao;
+    private final Dao<User> userDao;
+
+    public UserService(Dao<User> userDao) {
+        this.userDao = userDao;
+    }
 
     public List<User> findAll() {
         return userDao.findAll();
